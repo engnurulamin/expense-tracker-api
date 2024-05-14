2daec0712cd86af80832c4e5d74290781e43468d
Nurul Amin <namin2750@gmail.com>
2024-05-14 21:36:17 +0600

    mongodb connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,19 @@
-const mysql = require("mysql");
+const mongoose = require("mongoose");
+const { MONGODB_URL } = require("../secret");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "expense-tracker",
-});
-
-const connectDatabase = () => {
-  return new Promise((resolve, reject) => {
-    db.connect((err) => {
-      if (err) {
-        console.error("Error connecting to database:", err.message);
-        reject(err);
-      } else {
-        console.log("Database connected!");
-        resolve();
-      }
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-  });
+    console.log("Database Connected successfully");
+    mongoose.connection.on("error", (error) => {
+      console.error("Connection Error :", error);
+    });
+  } catch (error) {
+    console.error("DB Connection Fail:", error.toString());
+  }
 };
 
 module.exports = connectDatabase;
---
+++ src/secret.js
@@ -0,0 +1,6 @@
+let MONGO_LOCAL_URL = "mongodb://localhost:27017/expense-tracker";
+let MONGODB_URL =
+  "mongodb+srv://namin:namin1234@cluster0.srqbspp.mongodb.net/expense-tracker" ||
+  MONGO_LOCAL_URL;
+
+module.exports = { MONGODB_URL };
