9849dad6d84086a9ab19e463598f26c923a33716
Nurul Amin <namin2750@gmail.com>
2024-05-15 18:09:22 +0600

    dummy data for seeding

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  users: [
+    {
+      name: "Nurul Amin",
+      username: "nurulmin",
+      email: "amin@gmail.com",
+      password: "Amin@1234",
+    },
+    {
+      name: "Khairul Amin",
+      username: "kamin",
+      email: "kamin@gmail.com",
+      password: "Amin@1234",
+    },
+    {
+      name: "Mahia Ahmed",
+      username: "mahia",
+      email: "mahia@gmail.com",
+      password: "Amin@1234",
+    },
+  ],
+};
+
+module.exports = data;
