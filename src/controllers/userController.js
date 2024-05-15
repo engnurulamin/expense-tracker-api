b706ad75f6f37dcd2fd15e6cfcb7cc86814ca12e
Nurul Amin <namin2750@gmail.com>
2024-05-15 20:30:53 +0600

    pagination & search fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,11 +4,40 @@ const User = require("../models/userModel");
 
 const getUsers = async (req, res, next) => {
   try {
-    let users = await User.find({});
+    const search = req.query.search || "";
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 1;
+
+    const searchRegExp = new RegExp(".*" + search + ".* ", "i");
+    const filter = {
+      isAdmin: { $ne: true },
+      $or: [
+        { name: { $regex: searchRegExp } },
+        { username: { $regex: searchRegExp } },
+        { email: { $regex: searchRegExp } },
+      ],
+    };
+    const options = { password: 0 };
+    const users = await User.find(filter, options)
+      .limit(limit)
+      .skip((page - 1) * limit);
+
+    const count = await User.find(filter).countDocuments();
+
+    if (!users || users.length === 0) throw createError(404, "Users Not Found");
+
     return successResponse(res, {
       statusCode: 200,
-      message: "All users return successfully",
-      payload: { users },
+      message: "All users returned successfully",
+      payload: {
+        users,
+        pagination: {
+          totalPage: Math.ceil(count / limit),
+          currentPage: page,
+          previousPage: page - 1 > 0 ? page - 1 : null,
+          nextPage: page + 1 < Math.ceil(count / limit) ? page + 1 : null,
+        },
+      },
     });
   } catch (error) {
     next(error);
