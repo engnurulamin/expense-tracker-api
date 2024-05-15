2bc01e3e7461d134a1f80cff092596dd69ab3222
Nurul Amin <namin2750@gmail.com>
2024-05-15 19:34:38 +0600

    fetch all users working

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,7 @@ const User = require("../models/userModel");
 
 const getUsers = async (req, res, next) => {
   try {
+    let users = await User.find({});
     return successResponse(res, {
       statusCode: 200,
       message: "All users return successfully",
