01e9a37fc5de134abf6ba45628287e9f4d83490c
Nurul Amin <namin2750@gmail.com>
2024-05-20 20:19:17 +0600

    validation process

---
+++ src/validator/auth.js
@@ -0,0 +1,113 @@
+const { body } = require("express-validator");
+
+// Validate Registration
+const validateUserRegistration = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 3, max: 31 })
+    .withMessage("Name must be within 3-31 characters"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid Email address"),
+
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6, max: 20 })
+    .withMessage("Password must be at least 6 characters")
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .withMessage(
+      "Password should be within 6-20 characters. At least one uppercase, one lowercase, one number and a special character"
+    ),
+];
+
+// Validate Sign In
+const validateUserLogin = [
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid Email address"),
+
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6, max: 20 })
+    .withMessage("Password must be at least 6 characters")
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .withMessage(
+      "Password should be within 6-20 characters. At least one uppercase, one lowercase, one number and a special character"
+    ),
+];
+
+const validateChangePassword = [
+  body("oldPassword")
+    .trim()
+    .notEmpty()
+    .withMessage("Old Password is required")
+    .isLength({ min: 6, max: 20 })
+    .withMessage("Password must be at least 6 characters")
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .withMessage(
+      "Password should be within 6-20 characters. At least one uppercase, one lowercase, one number and a special character"
+    ),
+
+  body("newPassword")
+    .trim()
+    .notEmpty()
+    .withMessage("New Password is required")
+    .isLength({ min: 6, max: 20 })
+    .withMessage("New Password must be at least 6 characters")
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .withMessage(
+      "New Password should be within 6-20 characters. At least one uppercase, one lowercase, one number and a special character"
+    ),
+
+  body("confiremdPassword").custom((value, { req }) => {
+    if (value !== req.body.newPassword) {
+      throw new Error("New password did not match");
+    }
+    return true;
+  }),
+];
+
+const validateForgetPassword = [
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid Email address"),
+];
+
+const validateResetPassword = [
+  body("token").trim().notEmpty().withMessage("Token is missing"),
+
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6, max: 20 })
+    .withMessage("Password must be at least 6 characters")
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .withMessage(
+      "Password should be within 6-20 characters. At least one uppercase, one lowercase, one number and a special character"
+    ),
+];
+
+module.exports = {
+  validateUserRegistration,
+  validateUserLogin,
+  validateChangePassword,
+  validateForgetPassword,
+  validateResetPassword,
+};
