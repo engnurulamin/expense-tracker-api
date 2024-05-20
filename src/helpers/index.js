5c0e572c6aa869fd17431fde4be4c6188c643c90
Nurul Amin <namin2750@gmail.com>
2024-05-20 22:24:02 +0600

    coocies set helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -57,4 +57,29 @@ const jsonWebtoken = (payload, secretyKey, expiresIn) => {
   }
 };
 
-module.exports = { errorResponse, successResponse, getOne, jsonWebtoken };
+const accessTokenCookie = (res, accessToken) => {
+  res.cookie("accessToken", accessToken, {
+    maxAge: 5 * 60 * 1000,
+    httpOnly: true,
+    // secure: true,
+    sameSite: "none",
+  });
+};
+
+const refreshTokenCookie = (res, refreshToken) => {
+  res.cookie("refreshToken", refreshToken, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    // secure: true,
+    sameSite: "none",
+  });
+};
+
+module.exports = {
+  errorResponse,
+  successResponse,
+  getOne,
+  jsonWebtoken,
+  accessTokenCookie,
+  refreshTokenCookie,
+};
