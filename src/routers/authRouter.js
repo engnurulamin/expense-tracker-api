d9b019ef5912dd80053c51142385abed0c192128
Nurul Amin <namin2750@gmail.com>
2024-05-21 23:22:31 +0600

    more uses of middle weres

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { login, logout } = require("../controllers/authController");
 const { validateUserLogin } = require("../validator/auth");
 const runValidation = require("../validator");
+const { isLoggedOut, isLoggedIn } = require("../helpers/utils");
 
 const authRouter = express.Router();
 
-authRouter.post("/login", validateUserLogin, runValidation, login);
-authRouter.post("/logout", logout);
+authRouter.post("/login", validateUserLogin, runValidation, isLoggedOut, login);
+authRouter.post("/logout", isLoggedIn, logout);
 
 module.exports = authRouter;
