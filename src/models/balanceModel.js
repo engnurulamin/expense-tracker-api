6764e1a437a08ce798a625394cbb885954de3f59
Nurul Amin <namin2750@gmail.com>
2024-05-28 23:12:18 +0600

    add user fields for specific user's data

--- src/models/balanceModel.js
+++ src/models/balanceModel.js
@@ -15,6 +15,11 @@ const balanceSchema = new Schema(
       trim: true,
       maxlength: [255, "Max char can be 25"],
     },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   {
     timestamps: true,
